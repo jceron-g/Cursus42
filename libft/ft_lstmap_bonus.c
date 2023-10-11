5bd3a7c1492fd5d98cdee18b1341f3b0c7928d7a
Jose <138477551+jceron-g@users.noreply.github.com>
2023-10-11 10:51:12 +0200

    lstmap passed paco strict

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jceron-g <jceron-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 10:47:46 by jceron-g          #+#    #+#             */
-/*   Updated: 2023/09/29 11:26:49 by jceron-g         ###   ########.fr       */
+/*   Updated: 2023/10/11 10:48:54 by jceron-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;
 	t_list	*new_node;
 
+	if (!f || !del)
+		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		new_node = ft_lstnew(NULL);
 		if (!new_node)
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
-		lst = lst->next;
+		new_node->content = f(lst->content);
 		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
 	}
 	return (new_lst);
 }
