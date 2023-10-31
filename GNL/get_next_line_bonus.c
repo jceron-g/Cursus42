6f02ee0974dacbb86cd5859971dd43fa49036ba3
Jose <138477551+jceron-g@users.noreply.github.com>
2023-10-31 11:52:32 +0100

    Final project + bonus

---
+++ GNL/get_next_line_bonus.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jceron-g <jceron-g@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 12:19:51 by jceron-g          #+#    #+#             */
+/*   Updated: 2023/10/31 11:46:03 by jceron-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_read_line(int fd, char *aux_line)
+{
+	char	*buffer;
+	char	*save_aux;
+	int		read_bytes;
+
+	buffer = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	if (!buffer)
+		return (NULL);
+	read_bytes = 1;
+	while (!ft_strchr(buffer, '\n') && read_bytes > 0)
+	{
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(aux_line);
+			aux_line = NULL;
+			return (NULL);
+		}
+		save_aux = aux_line;
+		aux_line = ft_strjoin(save_aux, buffer, read_bytes);
+	}
+	free(buffer);
+	if (ft_strlen(aux_line) == 0)
+		return (free(aux_line), NULL);
+	return (aux_line);
+}
+
+char	*ft_get_line(char *aux_line)
+{
+	int		i;
+	int		j;
+	char	*get_line;
+
+	i = 0;
+	while (aux_line[i] != '\0' && aux_line[i] != '\n')
+		i++;
+	get_line = ft_calloc((i + 1 + (aux_line[i] == '\n')), sizeof(char));
+	if (!get_line)
+		return (NULL);
+	j = 0;
+	while (aux_line[j] != '\0' && aux_line[j] != '\n')
+	{
+		get_line[j] = aux_line[j];
+		j++;
+	}
+	if (aux_line[j] == '\n')
+		get_line[j++] = '\n';
+	get_line[j] = '\0';
+	return (get_line);
+}
+
+char	*ft_clean_line(char *aux_line)
+{
+	int		i;
+	int		j;
+	char	*new_aux_line;
+
+	i = 0;
+	while (aux_line[i] != '\0' && aux_line[i] != '\n')
+		i++;
+	if (ft_strlen(aux_line) - i <= 0)
+	{
+		free(aux_line);
+		aux_line = NULL;
+		return (NULL);
+	}
+	new_aux_line = ft_calloc(ft_strlen(aux_line) - i + 1, sizeof(char));
+	if (!new_aux_line)
+		return (NULL);
+	i++;
+	j = 0;
+	while (aux_line[i] != '\0')
+		new_aux_line[j++] = aux_line[i++];
+	free(aux_line);
+	aux_line = NULL;
+	return (new_aux_line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*aux_line[OPEN_MAX];
+	char		*final_line;
+
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+	{
+		free(aux_line[fd]);
+		aux_line[fd] = NULL;
+		return (NULL);
+	}
+	aux_line[fd] = ft_read_line(fd, aux_line[fd]);
+	if (!aux_line[fd])
+		return (NULL);
+	final_line = ft_get_line(aux_line[fd]);
+	aux_line[fd] = ft_clean_line(aux_line[fd]);
+	return (final_line);
+}
---
+++ GNL/get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jceron-g <jceron-g@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 12:19:25 by jceron-g          #+#    #+#             */
+/*   Updated: 2023/10/31 11:39:04 by jceron-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_read_line(int fd, char *aux_line);
+char	*ft_get_line(char *aux_line);
+char	*ft_clean_line(char *aux_line);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *aux_line, char *buffer, int read_bytes);
+int		ft_strlen(char *str);
+void	*ft_calloc(size_t count, size_t size);
+
+#endif
